adda60ad3fb29eef0f1374c5f1d483c844c2bd71
elvisimprsntr <elvisimprsntr@gmail.com>
2013-03-28 12:28:54 -0400

    RPi temp

--- lib/siriproxy-rpi.rb
+++ lib/siriproxy-rpi.rb
@@ -18,8 +18,8 @@ class SiriProxy::Plugin::RPi < SiriProxy::Plugin
 
   def get_temp
 	tempC = `vcgencmd measure_temp`.gsub(/[^\d\.]/, '').to_f
-	tempF = tempC * 9 / 5 + 32
-	say "Your Raspberry Pi is #{tempC} degrees C or #{tempF} degrees F."
+	tempF = sprintf("%.1f", tempC * 9 / 5 + 32)
+	say "Your Raspberry Pi is #{tempC}°C or #{tempF}°F."
   end
   
 ############# Initialization
